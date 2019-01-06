8bf2f668d9e1ab06b15a83a128f21f38d8c05d6a
varshini <varshu1112@gmail.com>
2019-01-07 00:28:21 +0530

    graph

---
+++ app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular6Proj';
+
+  pos = 'HL(0,1)';
+
+
+  constructor(){}
+
+  ngOnInit() {
+
+  }
+}
