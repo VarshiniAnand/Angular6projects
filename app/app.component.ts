50060f14152d38a3eae1a619a011ce45784e4064
VarshiniAnand <37431249+VarshiniAnand@users.noreply.github.com>
2019-01-10 15:57:44 +0530

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +13,19 @@ export class AppComponent {
   pos = 'HL(0,1)';
 
 
-  constructor(){}
+  constructor(private http: HttpClient){}
 
   ngOnInit() {
 
+  }
+  
+    getRequest(url: string, params ? : any): Observable < any > {
+    return this.http.get(url, params);
+  }
+
+  postRequest(url: string, request ? : any, params ? : any): Observable < any > {
+  
+    let req = JSON.stringify(request) ? JSON.stringify(request) : '';
+     return this.http.post(url, req, this.commonHttpHeaders());
   }
 }
